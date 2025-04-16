cdb8196af3f01f763f03ec5cb1f930d52d7b1218
onee-only <kimww0306@gmail.com>
2025-04-17 00:38:53 +0900

    application/http: Add TODO

--- application/http/doc.go
+++ application/http/doc.go
@@ -4,11 +4,20 @@
 //
 // - https://datatracker.ietf.org/doc/html/rfc9110
 //
-// - https://datatracker.ietf.org/doc/html/rfc9111
+// - TODO: https://datatracker.ietf.org/doc/html/rfc9111
 //
 // - https://datatracker.ietf.org/doc/html/rfc9112
 //
-// - https://datatracker.ietf.org/doc/html/rfc9113
+// - TODO: soon, https://datatracker.ietf.org/doc/html/rfc9113
 //
 // - TODO: https://datatracker.ietf.org/doc/html/rfc9114
 package http
+
+// Unimplemented features excluding above:
+// - proxy server.
+// - Expect header (100-continue).
+// - Most of semantic actions. Including redirects.
+// - Cookies: https://datatracker.ietf.org/doc/html/rfc6265
+// - Version handling. (Backward compatibility for HTTP/1.0)
+// - Negotiating transfer codings: https://datatracker.ietf.org/doc/html/rfc9112#section-4-9
+// - Read/Write Timeouts in client.
