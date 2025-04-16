26ff71c148de0ca19006263f8c91518b3c2735ca
onee-only <kimww0306@gmail.com>
2025-04-16 15:02:10 +0900

    transport: Add errors

--- transport/conn.go
+++ transport/conn.go
@@ -7,9 +7,8 @@ import (
 )
 
 var (
-	ErrConnClosed        = errors.New("connection is closed")
-	ErrConnListnerClosed = errors.New("conn listener is closed")
-	ErrDeadLineExceeded  = errors.New("deadline exceeded")
+	ErrConnClosed       = errors.New("connection is closed")
+	ErrDeadLineExceeded = errors.New("deadline exceeded")
 )
 
 type Conn interface {
@@ -24,10 +23,20 @@ type Conn interface {
 	SetWriteDeadLine(t time.Time)
 }
 
+var (
+	ErrAddrAlreadyInUse   = errors.New("address already in use")
+	ErrConnListenerClosed = errors.New("conn listener is closed")
+)
+
 type ConnListener interface {
 	Accept(ctx context.Context) (Conn, error)
 	Close() error
 }
+
+var (
+	ErrConnRefused    = errors.New("connection refused")
+	ErrNetUnreachable = errors.New("network is unreachable")
+)
 
 type ConnDialer interface {
 	Dial(ctx context.Context, addr Addr) (Conn, error)
