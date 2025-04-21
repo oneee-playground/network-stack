82adf2db00f5aaf9843aae66f6c8f7fbabdd8f13
onee-only <kimww0306@gmail.com>
2025-04-21 20:15:32 +0900

    session/tls: Add tls package

---
+++ session/tls/doc.go
@@ -0,0 +1,6 @@
+// Package tls imlements Transport Layer Security (TLS)
+//
+// NOTE: It only supports TLS 1.3
+//
+// Reference: https://datatracker.ietf.org/doc/html/rfc8446
+package tls
---
+++ session/tls/tls_test.go
@@ -0,0 +1,13 @@
+package tls
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewVersion(t *testing.T) {
+	v := NewVersion([2]uint8{0x12, 0x34})
+	assert.Equal(t, uint16(0x1234), v)
+	assert.Equal(t, []byte{0x12, 0x34}, v.Bytes())
+}
